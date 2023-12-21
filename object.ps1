6bef6597011723493e9ea6c493e7d1de05eff19d
Christopher Lloyd Jones <126833491+SoftwareStudent2023@users.noreply.github.com>
2023-12-21 15:48:58 -0500

    Add files via upload

---
+++ object.ps1
@@ -0,0 +1,12 @@
+new-object
+
+prompt 1 2 3 4 5 6 7 8 9 a b c d e f 10 11 12 13 14 15 16 17 18 19 1a 1b 1c 1d 1e 1f 20 21 22 23 24 25 26 27 28 29 2a 2b 2c 2d 2e 2f 30 31 32 33 34 35 36 37 38 39 3a 3b 3c 3d 3e 3f 40 00 1 10 11 100 101 110 111 1000 1001 1010 1011 1100 1101 1110 1111 10000 10001 10010 10011 10100 10101 10110 10111 11000 11001 11010 11011 11100 11101 11110 11111 00100000 00100001 00100010 00100011 00100100 00100101 00100110 00100111 00101000 0101001 00101010 00101011 00101100 00101101 00101110 00101111 00110000 00110001 00110010 00110011 00110100 00110101 00110110 00110111 00111000 00111001 00111010 00111011 00111100 00111101 00111110 00111111 01000000 0
+
+trace-command -trace
+trace-command -encode
+
+prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;prompt;  
+
+sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object; sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object; sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object; sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object;sort-object; 
+
+new-object 
